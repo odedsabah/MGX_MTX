f0a3a63ecc0a1d2b8e2b091523f15afd759dc363
odedsabah <62263087+odedsabah@users.noreply.github.com>
2022-05-04 11:19:49 +0300

    A circular graph

---
+++ CSM79HGP.MGX.coverage-stats.R
@@ -0,0 +1,94 @@
+
+#---
+#title: "Create graphs for figure 1 to thesis"
+#date: "4/5/2022"
+#input:  coverage Statistics of CSM79HGP.MGX
+#output:A circular graph showing the data distribution of samples from IBDMDB
+#---
+
+# Upload packages
+
+library(ggplot2)
+library(hrbrthemes)
+library(dplyr)
+library(tidyr)
+library(viridis)
+
+
+## input data table
+
+CSM79HGP_MGX <- read.table("/Users/odedsabah/Desktop/CSM79HGP.MGX.coverage-stats.txt")
+
+CSM79HGP_MTX <- read.table("/Users/odedsabah/Desktop/CSM79HGP.MTX.coverage-stats.txt")
+
+# clacolate retio between mean - median
+
+mean_MGX <- as.double(CSM79HGP_MGX$V5)
+median_MGX <- as.double(CSM79HGP_MGX$V4)
+Division_Mea_Med_MGX <- mean_MGX/median_MGX
+
+
+hist(mean_MGX)
+hist(median_MGX)
+hist(Division_Mea_Med_MGX)
+plot(Division_Mea_Med_MGX)
+
+
+mean_MTX <- as.double(CSM79HGP_MTX$V5)
+median_MTX <- as.double(CSM79HGP_MTX$V4)
+Division_Mea_Med_MTX <- mean_MTX[median_MTX != 0 ]/median_MTX[median_MTX != 0 ]
+vec = (Division_Mea_Med_MTX[complete.cases(Division_Mea_Med_MTX)])
+plot(vec, ylim = c(0,10))
+
+
+hist(mean_MTX)
+hist(median_MTX)
+
+# combin the columes to df
+com_vec <- cbind(Division_Mea_Med_MGX,Division_Mea_Med_MTX)
+com2df = as.data.frame(com_vec)
+
+set.seed(1)
+c2c1 <- pivot_longer(com2df,Division_Mea_Med_MGX:Division_Mea_Med_MTX)
+c2c1
+
+# Represent the Relationship distribution
+png('~/Downloads/x.png')
+c2c1 %>%
+  ggplot( aes(x=value, fill=name)) +
+    geom_histogram( color="#e9ecef", alpha=0.6, position = 'identity') +
+    scale_fill_manual(values=c("#69b3a2", "#404080")) +
+    theme_ipsum() +
+    xlim(c(0.5,2)) +
+    labs(x="Ratio between average and median", y="frequency") +
+    theme_minimal() +
+    theme(legend.position = "none") +
+    theme(panel.background = element_blank())
+dev.off()
+
+sample <- read.csv("/Users/odedsabah/Desktop/Book5 2.csv")
+mat = matrix(runif(36), 6)
+rownames(mat) = letters[1:6]
+colnames(mat) = letters[1:6]
+
+mat
+sample<- as.matrix(sample)
+rownames(sample) <- c("metabolomics","metagenomics","metatranscriptomics","proteomics")
+sample
+
+library(circlize)
+circos.par(gap.degree = 4)
+chordDiagram(sample, directional = TRUE, annotationTrack = "grid",
+  preAllocateTracks = list(list(track.height = 0.05),
+                           list(track.height = 0.05)))
+circos.trackPlotRegion(track.index = 1, panel.fun = function(x, y) {
+  xlim = get.cell.meta.data("xlim")
+  ylim = get.cell.meta.data("ylim")
+  sector.index = get.cell.meta.data("sector.index")
+  circos.text(mean(xlim), mean(ylim), sector.index, facing = "inside", niceFacing = TRUE)
+}, bg.border = NA)
+circos.trackPlotRegion(track.index = 2, panel.fun = function(x, y) {
+  circos.axis("bottom", major.tick.percentage = 0.2, labels.cex = 0.4)
+}, bg.border = NA)
+circos.clear()
+
---
+++ cal_cov.py
@@ -0,0 +1,78 @@
+#3!/usr/bin/python3
+
+
+# Upload packages
+import sys
+import pandas as pd
+
+#---
+#date: "5/4/2022"
+#input:  Ready Df for analysis Coverage MG/TX
+#output: DF with distances from the start to and in per contigs
+#---
+
+# MGX_coverage_stats = '/Users/odedsabah/Desktop/CSM79HGP.MGX.coverage-stats.txt'
+# MTX_coverage_stats = '/Users/odedsabah/Desktop/CSM79HGP.MTX.coverage-stats.txt'
+# gff_file = '/Users/odedsabah/Desktop/CSM79HGP.gff'
+# min_length = 900
+# max_length = 1100
+
+if len(sys.argv) != 5:
+        quit("\nUsage: " + sys.argv[0] + " <MGX-coverage-stats> <gff-file> <min-length> <max_length> \n")
+
+
+MGX_coverage_stats = sys.argv[1] #The input (MGX_coverage_stats) is output of cal_dist.py in test3
+gff_file = sys.argv[2]
+min_length = sys.argv[3]
+max_length = sys.argv[4]
+
+valid_contigs = []
+with open(gff_file) as fin:
+    for line in fin:
+        # k119_47955    Prodigal_v2.6.3 CDS     12678   13244   102.5   +       0       ID=1_9;partial=00;start_type=ATG;rbs_motif=None;rbs_spacer=None;gc_cont=0.568;conf=100.00;score=101.87>
+        fs = line.strip().split("\t")
+        if line[0] == "#" or fs[2] != "CDS":
+            continue
+        try:
+            (contig_length, length_read) = fs[0], (int(fs[4]) - (int(fs[3])))
+            if min_length <= int(length_read) <= max_length:
+                valid_contigs.append(contig_length)
+        except:
+            quit("Unexpected line: " + line)
+
+
+def MGX_C2_DF (MGX_coverage_stats, MTX_coverage_stats):
+    MGX_DF = pd.read_csv(MGX_coverage_stats, sep='\t')
+    # Contig Start	End	Median	Mean Sd	Min-cvg	Max-cvg	cvg.start+10 cvg.end-10
+    try:
+        MGX_DF = MGX_DF.loc[MGX_DF["Contig"].isin(valid_contigs), :]
+        MGX_DF["Start_200"] = MGX_DF["Start"] + 200
+        MGX_DF["End_300"] = MGX_DF["End"] - 300
+        MGX_DF["End_100"] = MGX_DF["End"] - 100
+        MGX_DF = MGX_DF.loc[:, ("Contig", "Start", "Start_200", "End_300", "End_100", "End")]
+
+        print(MGX_DF)
+
+    except:
+        print("Unexpected : " + MGX_DF["Contig"])
+
+    try:
+        MTX_DF = pd.read_csv(MTX_coverage_stats, sep='\t')
+        # Contig Start	End	Median	Mean Sd	Min-cvg	Max-cvg	cvg.start+10 cvg.end-10
+        MTX_DF = MTX_DF.loc[MTX_DF["Contig"].isin(valid_contigs), :]
+        MTX_DF["Start_200"] = MTX_DF["Start"] + 200
+        MTX_DF["End_300"] = MTX_DF["End"] - 300
+        MTX_DF["End_100"] = MTX_DF["End"] - 100
+        MTX_DF = MTX_DF.loc[:, ("Contig", "Start", "Start_200", "End_300", "End_100", "End")]
+
+        print(MTX_DF)
+        # MGX_DF.to_csv("/Users/odedsabah/Desktop/out_test.tsv", sep='\t', index= False)
+    except:
+        print("Unexpected : " + MTX_DF["Contig"])
+
+MGX_C2_DF(MGX_coverage_stats, MTX_coverage_stats)
+
+
+
+
+
