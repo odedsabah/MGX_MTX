aa2b364a4132b1cb2c8b3cfff4a99a566bccf5a8
odedsabah <62263087+odedsabah@users.noreply.github.com>
2022-09-20 15:31:50 +0300

    fix flies

--- braken_community_profiles/Species_Abundance.py
+++ braken_community_profiles/Species_Abundance.py
@@ -31,29 +31,32 @@ class Species_Abundance:
             name_by_point = name[6].split(".")[0]
             samples_braken = self.path_of_braken_output + "/" + sample # append the sample name to it path
             table_s = pd.read_csv(samples_braken, sep='\t')  # Read the table containing the frequencies of species
-            table_s = table_s.drop(table_s[table_s['fraction_total_reads'] <= self.abundance_threshold].index) # remove all species smaller than 1%
+            # table_s = table_s.drop(table_s[table_s['fraction_total_reads'] <= self.abundance_threshold].index)
             table_s = table_s.rename(columns={'fraction_total_reads': name_by_point})
             table_s = table_s.set_index('species_name').iloc[:,[6]]
             self.species_filter[name_by_point] = table_s
+            # print(self.species_filter[name_by_point])
 
 
     def create_set_species(self):
         species_set = set()
         for sample in os.listdir(self.path_of_braken_output):
-            with open(f'{self.path_of_braken_output}{sample}', "r") as R:
+            with open(f'{self.path_of_braken_output}/{sample}', "r") as R:
                 for line in R:
                     species_s = line.split("\t")[1]
                     species_set.add(species_s)
                 self.species_main = pd.DataFrame(index=species_set)
-        return self.species_main
+        # return self.species_main
+        # print (self.species_main)
 
     def marge2main(self):
         for table_s in self.species_filter.values():
+            # print(table_s)
             self.species_main = pd.concat([self.species_main, table_s], axis=1, join="outer")
-            #merge_tablas = pd.merge(self.species_main, self.species4index, on='species_name', how='outer')
+        print(self.species_main.shape)
         self.species_main = self.species_main.fillna(0).sort_index(0).sort_index(1)
-        print(self.species_main)
-        #return self.species_main.to_csv("/home/odeds/out_test.csv", index=True)
+        # print(self.species_main)
+        return self.species_main.to_csv("/home/odeds/ESCO_prediction/MGX_control.csv", index=True)
 
 
 def main():
